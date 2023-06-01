c0bf403a064625cd7d2d628cc9e87decf1fe7479
Lufrut <superkolasar5@gmail.com>
2023-06-01 14:07:51 +0200

    delete admin repository

--- src/main/java/com/pc/pcsearch/postgresql/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pc.pcsearch.postgresql.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
