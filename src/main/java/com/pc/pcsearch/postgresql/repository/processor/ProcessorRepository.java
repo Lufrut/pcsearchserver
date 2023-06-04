6ab3456d852acaea5e2c38db4810062b095385e6
Lufrut <superkolasar5@gmail.com>
2023-06-04 13:20:31 +0200

    fix issue with mismatch process class with processor class

--- src/main/java/com/pc/pcsearch/postgresql/repository/processor/ProcessorRepository.java
+++ src/main/java/com/pc/pcsearch/postgresql/repository/processor/ProcessorRepository.java
@@ -1,6 +1,7 @@
 package com.pc.pcsearch.postgresql.repository.processor;
 
+import com.pc.pcsearch.models.buildpc.processor.Processor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProcessorRepository extends JpaRepository<Process, Long> {
+public interface ProcessorRepository extends JpaRepository<Processor, Long> {
 }
