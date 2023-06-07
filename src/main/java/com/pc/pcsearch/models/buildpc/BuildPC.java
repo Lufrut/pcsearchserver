5fe87985242efc452ba56ac1d9ff78829ac09fc6
Lufrut <superkolasar5@gmail.com>
2023-06-07 10:55:00 +0200

    try10-

--- src/main/java/com/pc/pcsearch/models/buildpc/BuildPC.java
+++ src/main/java/com/pc/pcsearch/models/buildpc/BuildPC.java
@@ -27,7 +27,7 @@ public class BuildPC {
 
     private String nameOfBuild;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private User user;
 
     @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
